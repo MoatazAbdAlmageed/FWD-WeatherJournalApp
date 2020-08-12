f0c87916be870ede5202acd926a29e9c23aabc38
MoatazAbdAlmageed <moataz@wuilt.com>
2020-08-12 18:08:39 +0200

    add packages init server

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors);
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.listen(3000, () => {
+  console.log("🚀🚀🚀🚀🚀🚀 app running at port 3000");
+});
